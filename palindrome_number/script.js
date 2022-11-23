753e4a12e6df9de304dd939a3b5d6d55c9e7ae6c
Robbie Prokop <robbieprokop@gmail.com>
2022-11-23 14:19:51 +0400

    second answer for palindrom

--- palindrome_number/script.js
+++ palindrome_number/script.js
@@ -21,10 +21,28 @@
 // -231 <= x <= 231 - 1
 
 // Follow up: Could you solve it without converting the integer to a string?
-let x = 121;
+// let x = 121;
 
-const isPalindrome = (x) => {
-  console.log("x  :>> ", x);
+// const isPalindrome = (x) => {
+//   let output = false;
+//   let newX = Array.from(String(x)).reverse().join("");
+//   if (x === +newX) output = true;
+//   return output;
+// };
+
+// console.log(isPalindrome(x));
+
+let num = 4562;
+// Function to reverse digits of num
+const isPalindrome = (num) => {
+  let rev_num = 0;
+  let numCopy = num;
+  while (numCopy > 0) {
+    rev_num = rev_num * 10 + (numCopy % 10);
+    numCopy = Math.floor(numCopy / 10);
+  }
+  if (num === rev_num) return true;
+  return false;
 };
 
-console.log(isPalindrome(x));
+console.log(isPalindrome(num));
