c30451d198fbff1af5b392b6755902f3e35c257c
Robbie Prokop <robbieprokop@gmail.com>
2024-01-12 19:00:44 -0700

    Add arraySum and sum functions

---
+++ HackerRank/arraySum/arraySum.js
@@ -0,0 +1,16 @@
+// Given an array of integers, find the sum of its elements.
+
+// For example, if the array , , so return .
+
+// Function Description
+
+// Complete the simpleArraySum function in the editor below. It must return the sum of the array elements as an integer.
+
+// simpleArraySum has the following parameter(s):
+
+// ar: an array of integers
+
+
+function arraySum(arr) {
+  return arr.reduce((acc, cur) => acc + cur)
+}
