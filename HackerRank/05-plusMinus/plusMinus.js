f0e3242572b8d65de204b34e604ea59d2285eb98
Robbie Prokop <robbieprokop@gmail.com>
2024-01-16 21:14:02 -0700

    find initial solution

--- HackerRank/05-plusMinus/plusMinus.js
+++ HackerRank/05-plusMinus/plusMinus.js
@@ -1,4 +1,30 @@
 const plusMinus = (arr) => {
+  let positive = 0;
+  let negative = 0;
+  let zero = 0;
+  let count = arr.length;
+
+  arr.forEach(num => {
+
+    switch (true) {
+      case num > 0:
+        positive++;
+        break;
+      case num < 0:
+        negative++;
+        break;
+      case num === 0:
+        zero++;
+    }
+  })
+
+  const positiveRatio = (positive / count).toFixed(6)
+  const negativeRatio = (negative / count).toFixed(6)
+  const zeroRatio = (zero / count).toFixed(6)
+
+  console.log('positiveTotal :>> ', positiveRatio);
+  console.log('negativeTotal :>> ', negativeRatio);
+  console.log('zeroTotal :>> ', zeroRatio);
 
 }
 
