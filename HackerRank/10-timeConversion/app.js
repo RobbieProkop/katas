d6d92224af5b1827cda89279fc67e2b47a0cad5d
Robbie Prokop <robbieprokop@gmail.com>
2024-01-18 22:46:06 -0700

    second solution for time conversion

--- HackerRank/10-timeConversion/app.js
+++ HackerRank/10-timeConversion/app.js
@@ -1,17 +1,27 @@
+// const timeConversion = (s) => {
+
+//   const start = s.slice(0, 2)
+//   const minSec = s.slice(2, s.length - 2)
+//   if (s.includes("AM")) {
+//     if (s[0] === "1" && s[1] === "2") return "00" + minSec
+//     return start + minSec
+//   }
+//   if (s[0] === "1" && s[1] === "2") return "12" + minSec
+//   return (12 + Number(start)).toString() + minSec
+// }
 const timeConversion = (s) => {
 
-  const start = s.slice(0, 2)
-  const minSec = s.slice(2, s.length - 2)
-  if (s.includes("AM")) {
-    console.log("YES");
-    if (s[0] === "1" && s[1] === "2") return "00" + minSec
-    return start + minSec
+  let hour = Number(s.slice(0, 2));
+  const minSec = s.slice(2, s.length - 2);
+  const period = s.slice(8);
+
+  if (period === "AM") {
+    hour = hour === 12 ? "00" : hour.toString().padStart(2, "0");
+  } else {
+    hour = hour === 12 ? "12" : (hour + 12).toString()
   }
-
-  if (s[0] === "1" && s[1] === "2") return "12" + minSec
-  return (12 + Number(start)).toString() + minSec
-
+  return hour + minSec
 }
 
-const s = '11:01:00PM'
+const s = '01:01:00AM'
 console.log(timeConversion(s))
