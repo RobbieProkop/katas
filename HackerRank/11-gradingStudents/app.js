2381394f79bfc8100ad4f38ecafefe865c5f1e39
Robbie Prokop <robbieprokop@gmail.com>
2024-01-20 19:49:18 -0700

    partial answer

--- HackerRank/11-gradingStudents/app.js
+++ HackerRank/11-gradingStudents/app.js
@@ -1,8 +1,12 @@
 const gradingStudents = (grades) => {
   for (let i = 0; i < grades.length; i++) {
-
+    if (grades[i] < 38) continue
+    if (((grades[i] + 5) % 5) >= 3) {
+      grades[i] = grades[i] + (grades[i] % 5) - 1
+    }
   }
+  return grades
 }
 
 const grades = [75, 67, 38, 33]
-console.log(gradingStudents())
+console.log(gradingStudents(grades))
