bf8cebf8b5416c45d18b222d98260a41783ce056
Robbie Prokop <robbieprokop@gmail.com>
2024-01-20 19:56:49 -0700

    second solution removing while loop

--- HackerRank/11-gradingStudents/app.js
+++ HackerRank/11-gradingStudents/app.js
@@ -1,10 +1,20 @@
+// const gradingStudents = (grades) => {
+//   for (let i = 0; i < grades.length; i++) {
+//     if (grades[i] < 38) continue
+//     if (((grades[i] + 5) % 5) >= 3) {
+//       while(grades[i] % 5 !== 0){
+//         grades[i]
+//       }
+//     }
+//   }
+//   return grades
+// }
+
 const gradingStudents = (grades) => {
   for (let i = 0; i < grades.length; i++) {
     if (grades[i] < 38) continue
     if (((grades[i] + 5) % 5) >= 3) {
-      while (grades[i] % 5 !== 0) {
-        grades[i]++
-      }
+      grades[i] = Math.ceil(grades[i] / 5) * 5
     }
   }
   return grades
